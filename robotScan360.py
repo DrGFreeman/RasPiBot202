a06e10b7e21af2df54089e2e3e2103b4a8821f50
DrGFreeman <DrGFreeman@users.noreply.github.com>
2016-10-31 23:27:13 -0400

    Minor modifications

--- robotScan360.py
+++ robotScan360.py
@@ -3,9 +3,38 @@ import numpy as np
 import time
 import robotBuilder
 
+def scanTurnToMax():
+
+    turnSpeed = .4
+    timeOneRev = 2.3
+    nbPts = 30
+    tStep = timeOneRev / nbPts
+    data = np.array([[],[]])
+    dist = []
+    t = []
+    for step in range(nbPts + 2):
+        time.sleep(.3)
+        rpb202.readAStar()
+        dist.append(snrCtr.getObstDist())
+        t.append(step * tStep)
+        rpb202.turn(turnSpeed)
+        time.sleep(tStep)
+        rpb202.stop()
+
+    indexMax = np.argmax(dist)
+    print dist[indexMax]
+    tIndexMax = t[indexMax]
+    print indexMax
+
+    rpb202.turn(-turnSpeed)
+    time.sleep(t[-1] - tIndexMax)
+    rpb202.stop()
+
+    return t, dist
+
 def scan360(nbPts):
-    turnSpeed = .18
-    timeOneRev = 7.1
+    turnSpeed = .25
+    timeOneRev = 4.5
     thetaStep = 2 * np.pi / nbPts
     theta = np.pi / 2
     thetaPts = []
@@ -38,12 +67,13 @@ snrCtr = rpb202.sensors[2]
 
 try:
     ##  Perform scan
-    scanX, scanY = scan360(180)
 
-    plt.scatter(0, 0, c = 'r', s = 20)
-    plt.scatter(scanX, scanY)
-    plt.gca().set_aspect('equal')
-    plt.show()
+    scanT, scanD = scanTurnToMax()
+
+##    plt.scatter(0, 0, c = 'r', s = 20)
+##    plt.scatter(scanT, scanD)
+##    #plt.gca().set_aspect('equal')
+##    plt.show()
 
 except KeyboardInterrupt:
     rpb202.stop()
