bfd35a867e6cbf2a2655673c7e1e570e43d73848
DrGFreeman <DrGFreeman@users.noreply.github.com>
2016-11-08 22:28:10 -0500

    Added encoders support

---
+++ arduino/AStarRPiSlaveRPB202.ino
@@ -0,0 +1,135 @@
+#include <Servo.h>
+#include <AStar32U4.h>
+#include <PololuRPiSlave.h>
+#include <FastGPIO.h>
+
+/* This example program shows how to make the A-Star 32U4 Robot
+ * Controller into a Raspberry Pi I2C slave.  The RPi and A-Star can
+ * exchange data bidirectionally, allowing each device to do what it
+ * does best: high-level programming can be handled in a language such
+ * as Python on the RPi, while the A-Star takes charge of motor
+ * control, analog inputs, and other low-level I/O.
+ *
+ * The example and libraries are available for download at:
+ *
+ * https://github.com/pololu/pololu-rpi-slave-arduino-library
+ *
+ * You will need the corresponding Raspberry Pi code, which is
+ * available in that repository under the pi/ subfolder.  The Pi code
+ * sets up a simple Python-based web application as a control panel
+ * for your Raspberry Pi robot.
+ */
+
+// Custom data structure that we will use for interpreting the buffer.
+// We recommend keeping this under 64 bytes total.  If you change the
+// data format, make sure to update the corresponding code in
+// a_star.py on the Raspberry Pi.
+
+struct Data
+{
+  bool yellow, green, red;
+  bool buttonA, buttonB, buttonC;
+
+  int16_t leftMotor, rightMotor;
+  uint16_t batteryMillivolts;
+  uint16_t analog[6];
+
+  bool resetEncoders;
+
+  int16_t encoderLCount, encoderRCount;
+
+  bool playNotes;
+  char notes[14];
+};
+
+PololuRPiSlave<struct Data,0> slave;
+PololuBuzzer buzzer;
+AStar32U4Motors motors;
+AStar32U4ButtonA buttonA;
+AStar32U4ButtonB buttonB;
+AStar32U4ButtonC buttonC;
+
+// Encoders variables
+const byte encoderLeftPin = 8;
+const byte encoderRightPin = 7;
+volatile uint16_t encoderLeftCount;
+volatile uint16_t encoderRightCount;
+
+// ISRs
+// ISR for left encoder (PCINT)
+ISR(PCINT0_vect)
+{
+  encoderLeftCount++;
+}
+
+// ISR for right encoder (INT)
+static void rightISR()
+{
+  encoderRightCount++;
+}
+
+void setup()
+{
+  // Set up the slave at I2C address 20.
+  slave.init(20);
+
+  // Play startup sound.
+  buzzer.play("v10>>g16>>>c16");
+
+  // Setup encoders pins and attach ISRs
+  FastGPIO::Pin<encoderLeftPin>::setInputPulledUp();
+  FastGPIO::Pin<encoderRightPin>::setInputPulledUp();
+  PCICR = (1 << PCIE0);
+  PCMSK0 = (1 << PCINT4);
+  PCIFR = (1 << PCIF0); 
+  attachInterrupt(digitalPinToInterrupt(encoderRightPin), rightISR, CHANGE);
+}
+
+void loop()
+{
+  // Call updateBuffer() before using the buffer, to get the latest
+  // data including recent master writes.
+  slave.updateBuffer();
+
+  // Write various values into the data structure.
+  slave.buffer.buttonA = buttonA.isPressed();
+  slave.buffer.buttonB = buttonB.isPressed();
+  slave.buffer.buttonC = buttonC.isPressed();
+
+  // Change this to readBatteryMillivoltsLV() for the LV model.
+  slave.buffer.batteryMillivolts = readBatteryMillivoltsSV();
+
+  for(uint8_t i=0; i<6; i++)
+  {
+    slave.buffer.analog[i] = analogRead(i);
+  }
+  
+  // Read encoders
+  slave.buffer.encoderLCount = encoderLeftCount;
+  slave.buffer.encoderRCount = encoderRightCount;
+  if (slave.buffer.resetEncoders)
+  {
+    slave.buffer.resetEncoders = 0;
+    encoderLeftCount = 0;
+    encoderRightCount = 0;
+  }
+
+  // READING the buffer is allowed before or after finishWrites().
+  ledYellow(slave.buffer.yellow);
+  ledGreen(slave.buffer.green);
+  ledRed(slave.buffer.red);
+  motors.setSpeeds(slave.buffer.leftMotor, slave.buffer.rightMotor);
+
+  // Playing music involves both reading and writing, since we only
+  // want to do it once.
+  if(slave.buffer.playNotes)
+  {
+    buzzer.play(slave.buffer.notes);
+    while(buzzer.isPlaying());
+    slave.buffer.playNotes = false;
+  }
+
+  // When you are done WRITING, call finalizeWrites() to make modified
+  // data available to I2C master.
+  slave.finalizeWrites();
+}
