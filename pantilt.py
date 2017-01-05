a8d232e8d689650f5ff80edbb5e3d6f75cc736c5
DrGFreeman <DrGFreeman@users.noreply.github.com>
2017-01-05 09:36:08 -0500

    Tuned limits and added .down method

--- pantilt.py
+++ pantilt.py
@@ -5,9 +5,18 @@ class PanTilt:
     def __init__(self, panPin, tiltPin):
         self.panPin = panPin
         self.tiltPin = tiltPin
-        self.pan = Servo(self.panPin)
-        self.tilt = Servo(self.tiltPin)
+        self.pan = Servo(self.panPin, pwMin=.0008,
+                         pwCtr=.00157, pwMax=.0022, dir=1)
+        self.tilt = Servo(self.tiltPin, pwMin=.0008,
+                          pwCtr=.00143, pwMax=.00207, dir=-1)
 
+    def center(self):
+        self.pan.center()
+        self.tilt.center()
+
+    def down(self):
+        self.setPanTilt(0, -1)
+        
     def setPanTilt(self, pan, tilt):
         self.pan.move(pan)
         self.tilt.move(tilt)
@@ -16,9 +25,6 @@ class PanTilt:
         self.pan.sweep()
         self.tilt.sweep()
 
-    def center(self):
-        self.pan.center()
-        self.tilt.center()
 
 
 
