f1f9be57ffb210989ffd76257b5bc2fa2c5f4ca2
DrGFreeman <DrGFreeman@users.noreply.github.com>
2017-01-08 10:48:19 -0500

    Changed tilt servo pin number

--- robot.py
+++ robot.py
@@ -17,7 +17,7 @@ class Robot:
         self.buttons = []
         self.analog = [0, 0, 0, 0, 0, 0]
         self.camera = []
-        self.panTilt = PanTilt(12, 18)
+        self.panTilt = PanTilt(12, 13)
 
     def addSensor(self, sensorObj):
         self.sensors.append(sensorObj)
